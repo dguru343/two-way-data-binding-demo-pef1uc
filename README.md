1de25b45c81279cd8c2b0240a33a9001a0ac9b3c
dguru343 <kannanguru1999@gmail.com>
2022-02-16 16:23:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# two-way-data-binding-demo-pef1uc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/two-way-data-binding-demo-pef1uc)
---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnChanges, DoCheck } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <input [(ngModel)]="name" placeholder="type here" type="text">
+    <h3>{{name}}</h3>
+  `
+})
+export class AppComponent implements DoCheck  {
+  name = '';
+
+  ngDoCheck() {
+    console.log('Running change detection ', Date.now());
+  }
+}
